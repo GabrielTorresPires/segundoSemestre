b77a5c314f6b789d0f23314b198befd592b35c58
Gabriel Torres <gabrieltlpires@gmail.com>
2023-02-15 19:32:33 -0300

    Adiciona primeiras linhas de código

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string nome;
+int idade;
+
+
+
+    Console.WriteLine("Bem vindo ao meu primeiro projeto de 2023.");
+    Console.WriteLine("Por favor digite seu nome: ");
+    nome = Console.ReadLine()!;
+
+    Console.WriteLine("Por favor digite sua idade: ");
+    idade = Convert.ToInt32(Console.ReadLine());
+
+ Console.Clear();   
+
+    if (idade >= 18)
+        {
+        Console.WriteLine($"Seja bem vindo {nome}! ");
+        }
+
+    else 
+
+    Console.WriteLine("IDADE NÃO PERMITIDA!");
